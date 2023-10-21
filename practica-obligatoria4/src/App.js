b4b6c02ca856f6432c9826db1ed08c9687568bc9
JavierTonini <116577266+JavierTonini@users.noreply.github.com>
2023-10-21 19:44:10 -0300

    EntregaPracticaobligatoria4

---
+++ practica-obligatoria4/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login/Login";
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const showLoginHandler = () => {
+    setShowLogin(!showLogin);
+  };
+
+  const task = [];
+
+  return (
+    <div className="App">
+      {showLogin === true && (
+        <button type="button" onClick={showLoginHandler}>
+          Iniciar sesión
+        </button>
+      )}
+      {showLogin === false && <Login task={task} />}
+      {showLogin === false && (
+        <button type="button" onClick={showLoginHandler}>
+          Cerrar sesión
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default App;
